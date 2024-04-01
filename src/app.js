45abbd12006de4bf3a0e0005acc09b9915c45820
bildungsromanx <159861856+bildungsromanx@users.noreply.github.com>
2024-04-01 03:19:57 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,38 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["The", "Her", "His", "Our"];
+  let adj = [
+    "-itty-bitty",
+    "-fat",
+    "-funky",
+    "-sickly",
+    "-manic",
+    "-curious",
+    "-hungry"
+  ];
+  let noun = ["-teddybear", "-vampire", "-cat", "-toenail"];
+  let domain = [".com", ".net", ".edu"];
+
+  let concatenatedString = "";
+
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        for (let d = 0; d < domain.length; d++) {
+          concatenatedString +=
+            pronoun[a] + adj[b] + noun[c] + domain[d] + "<br>";
+        }
+      }
+    }
+  }
+  console.log(concatenatedString);
+
+  const button1 = document.getElementById("button");
+  const buttonResult = document.getElementById("buttonResult");
+
+  document.getElementById("button").onclick = () => {
+    buttonResult.innerHTML = concatenatedString;
+  };
+  return concatenatedString;
 };
